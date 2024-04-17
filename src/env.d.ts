1683ca033c03e4e47da1b5567cf3f97b7907cd1b
Oliver Speir <oliverspeir9@gmail.com>
2024-04-17 02:00:31 -0600

    add theme global

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,3 +2,12 @@
 /// <reference path="../.astro/types.d.ts" />
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference types="astro/client" />
+
+interface Window {
+	theme: {
+		setTheme: (theme: "auto" | "dark" | "light") => void;
+		getTheme: () => "auto" | "dark" | "light";
+		getSystemTheme: () => "light" | "dark";
+		getDefaultTheme: () => "auto" | "dark" | "light";
+	};
+}
